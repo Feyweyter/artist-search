fd242fdf7d8387d012913f629e81f94f1a102d68
Maria Frolova <mfrolova@roox.ru>
2020-08-31 22:45:22 +0300

    Added tests for actions and reducer

--- src/modules/ArtistSearch/ArtistSearch.test.js
+++ src/modules/ArtistSearch/ArtistSearch.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Input from '@material-ui/core/Input';
 import configureStore from 'redux-mock-store';
-import {shallow, mount} from "enzyme";
+import {mount} from "enzyme";
 import {Provider} from "react-redux";
 
 import ArtistSearch from './index';
 import ArtistList from './components/ArtistList';
+import actions from './effects/actions';
+import reducer from './effects/reducer';
 
 describe('ArtistList with items', () => {
     const initialState = {
@@ -57,5 +59,79 @@ describe('ArtistList with items', () => {
     it('<Input/> should have necessary props', () => {
         const foundProps = container.find(Input).props();
         expect(foundProps.value).toEqual('l');
+    });
+
+    it('check action on dispatching', () => {
+        let action;
+        store.dispatch(actions.getArtistsRequest('la'));
+        action = store.getActions();
+        expect(action[0].type).toBe("GET_ARTISTS_REQUEST");
+        expect(action[0].payload).toBe("la");
+    });
+
+    it('check action on input change', () => {
+        const event = {
+            preventDefault() {
+            },
+            target: {value: 'ga'}
+        };
+        container.find(Input).find('input').simulate('change', event);
+        const foundProps = container.find(Input).props();
+        expect(foundProps.value).toEqual('ga');
+        const action = store.getActions();
+        expect(action[0].type).toBe("GET_ARTISTS_REQUEST");
+        expect(action[0].payload).toBe("ga");
+    });
+
+    it('test reducer request action', () => {
+        let state = {...initialState.ArtistSearch};
+        const payload = 'cher';
+        state = reducer(state, {type: 'GET_ARTISTS_REQUEST', payload});
+        expect(state).toEqual({...initialState.ArtistSearch, isLoading: true});
+    });
+
+    it('test reducer success action', () => {
+        let state = {...initialState.ArtistSearch};
+        const payload = {
+            artistmatches: {
+                artist: [{
+                    name: "Lady Gaga",
+                    listeners: "4100355",
+                    mbid: "650e7db6-b795-4eb5-a702-5ea2fc46c848",
+                    url: "https://www.last.fm/music/Lady+Gaga",
+                    streamable: "0",
+                }, {
+                    name: "Gorillaz",
+                    listeners: "3781218",
+                    mbid: "e21857d5-3256-4547-afb3-4b6ded592596",
+                    url: "https://www.last.fm/music/Gorillaz",
+                    streamable: "0"
+                }],
+            },
+            'opensearch:Query': {
+                searchTerms: "la",
+                startPage: "1",
+            },
+            'opensearch:totalResults': "2"
+        };
+        state = reducer(state, {type: 'GET_ARTISTS_SUCCESS', payload});
+        expect(state).toEqual({
+            list: [{
+                name: "Lady Gaga",
+                listeners: "4100355",
+                mbid: "650e7db6-b795-4eb5-a702-5ea2fc46c848",
+                url: "https://www.last.fm/music/Lady+Gaga",
+                streamable: "0",
+            }, {
+                name: "Gorillaz",
+                listeners: "3781218",
+                mbid: "e21857d5-3256-4547-afb3-4b6ded592596",
+                url: "https://www.last.fm/music/Gorillaz",
+                streamable: "0"
+            }],
+            isLoading: false,
+            query: 'la',
+            page: 1,
+        });
     });
 });
