c73d5fbb22bd1d710342bcb3ea0c2d8990c42ad8
Maria Frolova <mfrolova@roox.ru>
2020-09-01 00:00:16 +0300

    Added small fixes to tests

--- src/modules/ArtistSearch/ArtistSearch.test.js
+++ src/modules/ArtistSearch/ArtistSearch.test.js
@@ -9,30 +9,54 @@ import ArtistList from './components/ArtistList';
 import actions from './effects/actions';
 import reducer from './effects/reducer';
 
-describe('ArtistList with items', () => {
-    const initialState = {
-        ArtistSearch: {
-            list: [{
-                name: "Lady Gaga",
-                listeners: "4100355",
-                mbid: "650e7db6-b795-4eb5-a702-5ea2fc46c848",
-                url: "https://www.last.fm/music/Lady+Gaga",
-                streamable: "0",
-            }, {
-                name: "Gorillaz",
-                listeners: "3781218",
-                mbid: "e21857d5-3256-4547-afb3-4b6ded592596",
-                url: "https://www.last.fm/music/Gorillaz",
-                streamable: "0"
-            }],
-            isLoading: false,
-            query: 'l',
-            page: 1,
-        }
-    };
-    const mockStore = configureStore();
-    let store, container;
+const initialState = {
+    ArtistSearch: {
+        list: [{
+            name: "Lady Gaga",
+            listeners: "4100355",
+            mbid: "650e7db6-b795-4eb5-a702-5ea2fc46c848",
+            url: "https://www.last.fm/music/Lady+Gaga",
+            streamable: "0",
+        }, {
+            name: "Gorillaz",
+            listeners: "3781218",
+            mbid: "e21857d5-3256-4547-afb3-4b6ded592596",
+            url: "https://www.last.fm/music/Gorillaz",
+            streamable: "0"
+        }],
+        isLoading: false,
+        query: 'l',
+        page: 1,
+    }
+};
 
+const mockStore = configureStore();
+let store, container;
+
+const mockJSON = {
+    artistmatches: {
+        artist: [{
+            name: "Lady Gaga",
+            listeners: "4100355",
+            mbid: "650e7db6-b795-4eb5-a702-5ea2fc46c848",
+            url: "https://www.last.fm/music/Lady+Gaga",
+            streamable: "0",
+        }, {
+            name: "Gorillaz",
+            listeners: "3781218",
+            mbid: "e21857d5-3256-4547-afb3-4b6ded592596",
+            url: "https://www.last.fm/music/Gorillaz",
+            streamable: "0"
+        }],
+    },
+    'opensearch:Query': {
+        searchTerms: "la",
+        startPage: "1",
+    },
+    'opensearch:totalResults': "2"
+};
+
+describe('<ArtistSearch/>', () => {
     beforeEach(() => {
         store = mockStore(initialState);
         container = mount(<Provider store={store}><ArtistSearch/></Provider>);
@@ -61,14 +85,6 @@ describe('ArtistList with items', () => {
         expect(foundProps.value).toEqual('l');
     });
 
-    it('check action on dispatching', () => {
-        let action;
-        store.dispatch(actions.getArtistsRequest('la'));
-        action = store.getActions();
-        expect(action[0].type).toBe("GET_ARTISTS_REQUEST");
-        expect(action[0].payload).toBe("la");
-    });
-
     it('check action on input change', () => {
         const event = {
             preventDefault() {
@@ -82,6 +98,20 @@ describe('ArtistList with items', () => {
         expect(action[0].type).toBe("GET_ARTISTS_REQUEST");
         expect(action[0].payload).toBe("ga");
     });
+});
+
+describe('Test actions and reducer', () => {
+    beforeEach(() => {
+        store = mockStore(initialState);
+    });
+
+    it('check action on dispatching', () => {
+        let action;
+        store.dispatch(actions.getArtistsRequest('la'));
+        action = store.getActions();
+        expect(action[0].type).toBe("GET_ARTISTS_REQUEST");
+        expect(action[0].payload).toBe("la");
+    });
 
     it('test reducer request action', () => {
         let state = {...initialState.ArtistSearch};
@@ -92,29 +122,7 @@ describe('ArtistList with items', () => {
 
     it('test reducer success action', () => {
         let state = {...initialState.ArtistSearch};
-        const payload = {
-            artistmatches: {
-                artist: [{
-                    name: "Lady Gaga",
-                    listeners: "4100355",
-                    mbid: "650e7db6-b795-4eb5-a702-5ea2fc46c848",
-                    url: "https://www.last.fm/music/Lady+Gaga",
-                    streamable: "0",
-                }, {
-                    name: "Gorillaz",
-                    listeners: "3781218",
-                    mbid: "e21857d5-3256-4547-afb3-4b6ded592596",
-                    url: "https://www.last.fm/music/Gorillaz",
-                    streamable: "0"
-                }],
-            },
-            'opensearch:Query': {
-                searchTerms: "la",
-                startPage: "1",
-            },
-            'opensearch:totalResults': "2"
-        };
-        state = reducer(state, {type: 'GET_ARTISTS_SUCCESS', payload});
+        state = reducer(state, {type: 'GET_ARTISTS_SUCCESS', payload: mockJSON});
         expect(state).toEqual({
             list: [{
                 name: "Lady Gaga",
@@ -135,3 +143,4 @@ describe('ArtistList with items', () => {
         });
     });
 });
+
